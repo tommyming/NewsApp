4aa8877b389736fc47f6bd649fd3ab450d2c182d
Tommy Han <tommyhonym@gmail.com>
2024-06-23 11:10:10 +0800

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Tommy Han on 23/6/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
